6421e884342787d97003d8c7f902c964bbc35702
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-04-08 19:34:20 -0300

    aa

--- Exercícios 02/Algoritmo 88.py
+++ Exercícios 02/Algoritmo 88.py
@@ -2,20 +2,14 @@ print("CALCULADORA")
 print("+ p/ Soma, - p/ Subtração, * p/ Multiplicação, / p/ Divisão")
 e = input("Digite sua escolha: ")
 txt = "Digite o {}º número: "
+a = float(input(txt.format(1)))
+b = float(input(txt.format(2)))
 if e == '+':
-    a = float(input(txt.format(1)))
-    b = float(input(txt.format(2)))
     print("A soma é", a + b)
 elif e == '-':
-    a = float(input(txt.format(1)))
-    b = float(input(txt.format(2)))
     print("A subtração é", a - b)
 elif e == '*':
-    a = float(input(txt.format(1)))
-    b = float(input(txt.format(2)))
     print("A multiplicação é", a * b)
 elif e == '/':
-    a = float(input(txt.format(1)))
-    b = float(input(txt.format(2)))
     print("A divisão é", a / b)
 else: print("Digite uma entrada válida!")
